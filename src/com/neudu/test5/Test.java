f3e1af0d951a8ec0eda29d15d7df93a42e504230
zhangwei <1875170993@qq.com>
2020-02-18 19:53:37 +0800

    学生表测试

---
+++ src/com/neudu/test5/Test.java
@@ -0,0 +1,11 @@
+package com.neudu.test5;
+
+import com.neudu.web.StudentWeb;
+
+public class Test {
+    public static void main(String[] args) {
+        StudentWeb studentWeb = new StudentWeb();
+        studentWeb.showmain();
+        studentWeb.input();
+    }
+}
